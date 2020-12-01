e194d43c60a8b61e4ebe23f3ccbf1971f8e5521e
annafisch <47110022+annafisch@users.noreply.github.com>
2020-12-01 10:27:49 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -66,8 +66,7 @@ class FlaskTests(unittest.TestCase):
 			"form_type": "delete_user"
 		}
 		responce = requests.post('http://localhost:5000', data=params)
-		self.assertEqual(responce.status_code, 500)
-		#self.assertEqual(responce.status_code, 200)
+		self.assertEqual(responce.status_code, 200)
 		#self.assertEqual(responce.content, 'success'.encode())
 		
 if __name__ == '__main__':
